9f88b954ce47d31ee4046befd2dc84c19127210b
Jana Haganová <jhaganova@gmail.com>
2022-04-15 15:37:12 +0200

    checks length, division by 11 validity

--- bonus.js
+++ bonus.js
@@ -13,27 +13,63 @@ function checkValidity() {
 }
 
 function isValid(input) {
-    if (input.length < 9) {
-        showResult('Číslo je príliš krátké.');
-        return;
+    if (checkLength(input) == false) {
+        return false;
     }
-
-    let isLong = input.length > 10;
-
 
     let isOld = input.length == 9;
 
+    if (!isOld) {
+        if (checkDivisionByEleven(input) == false) {
+            return false;
+        }
+    }
 
+    checkDateValidity(input);
 
 }
 
-function showResult(result) {
-    document.querySelector('#result > span').innerHTML = result;
+function checkLength(input) {
+    if (input.length < 9) {
+        showResult('Číslo je příliš krátké.', 'red');
+        return false;
+    }
+
+    if (input.length > 10) {
+        showResult('Číslo je příliš dlouhé.', 'red');
+        return false;
+    }
+
+    return true;
+}
+
+function checkDivisionByEleven(input) {
+    let inputAsNumber = parseInt(input);
+    if ((inputAsNumber % 11) != 0) {
+        showResult('Číslo je chybné.', 'red');
+        return false;
+    }
+
+    return true;
+}
+
+function checkDateValidity(input) {
+
+}
+
+
+
+function showResult(result, color) {
+    let resultSpan = document.querySelector('#result > span');
+    resultSpan.innerHTML = result;
+    resultSpan.style.color = color;
 }
 
 
 document.querySelectorAll('input').forEach((element) => {
     element.addEventListener('change', checkValidity);
 });
+
+
 
 //if result = OK, color green pls
